f8064a06d80cc5584bff5fbdb3d402283c39fa42
xuan thang <xuanthangsn@gmail.com>
2024-03-19 21:14:14 +0700

    delete redundant code

--- src/app.js
+++ src/app.js
@@ -5,47 +5,21 @@ const appRoute = require("../routes/index.js");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 
-const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { isAuthenticated } = require("../middlewares/isAuthenticated.js");
-const isAuthorized = require("../middlewares/isAuthorized.js");
-const { body, validationResult, matchedData} = require("express-validator");
-
-const knex = require("../db.js");
 
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.use("/test",[body('*.name').exists(), body('*.age').exists()],async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json(errors);
-  }
-  const data = matchedData(req);
-  console.log(data);
-  res.json(data);
-});
-
 app.use("/api", appRoute);
 
-app.use("/", (err, req, res, next) => {
-  console.log("enter err catcher");
-  console.log(err);
-  res.send("hahaha");
+// error handle middleware
+// if the precedent middleware call next(err), the request will be handled by this middleware right afterward 
+app.use((err, req, res, next) => {
+  const status = err.statusCode || 500;
+  const message = err.message || "Something is broken";
+  return req.status(status).json({ message });
 });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
-const sample = {
-  id: {
-    lt: 100,
-    gt: 10,
-  },
-  firstname: { eq: "xuanthang" },
-  email: { eq: "xuanthangsn@gmail.com" },
-  gender: { lt: "fe fucking male", gt: "wrong value" },
-  email_verified: { eq: false },
-};
